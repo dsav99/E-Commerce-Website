bcff6ddc96aeb69a7103ed7b1ae7ff9c12e004ad
Savi Dhillon <savidsav99@gmail.com>
2022-10-10 09:07:21 -0700

    Final amazon clone complete

--- amazon-clone/src/components/Home.js
+++ amazon-clone/src/components/Home.js
@@ -1,73 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../Css/Home.css"
 import Product from './Product'
 import cover from '../cover.jpeg'
-import book from "../harry.jpg"
-import axios from "axios";
+
 
 function Home() {
 
-    const options = {
-        method: 'GET',
-        url: 'https://amazon24.p.rapidapi.com/api/todaydeals',
-        headers: {
-          'X-RapidAPI-Key': 'e64b538230msh00bbd2aa8b989fdp152058jsn05f4d224c600',
-          'X-RapidAPI-Host': 'amazon24.p.rapidapi.com'
-        }
-      };
-
-      axios.request(options).then(function (response) {
-        console.log(response.data);
-    }).catch(function (error) {
-        console.error(error);
-    });
 
     return (
         <div className="home">
             <div className="home-container" >
-                <img className="home-image" src={cover} />
+                {/* <img className="home-image" src={"https://m.media-amazon.com/images/I/61DUO0NqyyL._SX3000_.jpg"} /> */}
 
                 <div className="home-row">
                     <Product 
-                    id="4903850"
-                    title="Harry Potter" 
+                    id="B08P3QVFMK"
+                    title="LED Flashlight Gloves, Gifts for Men Him Dad Boyfriend, Cool Gadget Hands-Free Lights for Camping Fishing Repairing, Fathers Day Christmas Birthday Gift (add Electronics)"
                     price={19.99} 
-                    image={book} 
+                    image={"https://m.media-amazon.com/images/I/41mSmuFaQIL.jpg"} 
                     rating={5}/>
                     <Product 
-                    id="4903850"
-                    title="Harry Potter" 
-                    price={19.99} 
-                    image={book} 
+                    id="B00652G4TS"
+                    title="BlueDriver Pro OBD2 Bluetooth Car Scan Tool and Code Reader" 
+                    price={79.00} 
+                    image={"https://m.media-amazon.com/images/I/41-PrU1phUL.jpg"} 
                     rating={5}/>
                 </div>
                 <div className="home-row">
                 <Product 
-                    id="4903851"
-                    title="Harry Potter" 
-                    price={19.99} 
-                    image={book} 
+                    id="B08SWD2SCK"
+                    title="Amazon Fire TV 43-inch 4-Series 4K UHD Smart TV" 
+                    price={199.99} 
+                    image={"https://m.media-amazon.com/images/I/41y0yqZPU8L.jpg"} 
                     rating={5}/>
                     <Product 
-                    id="4903850"
-                    title="Harry Potter" 
-                    price={19.99} 
-                    image={book} 
+                    id="B07P989QTJ"
+                    title="Prime members save on Luna Gaming Controller" 
+                    price={49.99} 
+                    image={"https://m.media-amazon.com/images/I/31rZcGd+U2L.jpg"} 
                     rating={5}/>
                     <Product 
-                    id="4903850"
-                    title="Harry Potter" 
-                    price={19.99} 
-                    image={book} 
+                    id="B08VJ5PCM8"
+                    title="LUXHMOX Double Long Soft Rifle Case, American Classic Outdoor Tactical Carbine Rifle Bag "
+                    price={67.96} 
+                    image={"https://m.media-amazon.com/images/I/51q0E4YxmhS.jpg"} 
                     rating={5}/>
 
                 </div>
                 <div className="home-row">
                 <Product 
                     id="4903852"
-                    title="Harry Potter" 
-                    price={19.99} 
-                    image={book} 
+                    title="Toshiba 4K Smart Fire TV with Far Field Technology" 
+                    price={699.99} 
+                    image={"https://m.media-amazon.com/images/I/512k25-6LdL.jpg"} 
                     rating={5}/>
                 </div>
             </div>
